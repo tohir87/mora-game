abc720b11e04f7381cbf18a39ebfcb9b6c9ec635
Tohir Omoloye <omoloye.tohir@yahoo.co.uk>
2018-04-18 16:14:19 +0100

    MoraApp class

---
+++ MoraApp.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class MoraApp {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner input;
+		input = new Scanner(System.in);
+
+		int roundWon = 0;
+		int roundLost = 0;
+		
+		while(true) {
+			// Instantiate mora class
+			Mora obj = new Mora();
+			
+			int gameRound = 0;
+			
+			boolean gameState = true;
+			
+			// select player type. evens or odds 
+			boolean isOkay = obj.selectPlayerType();
+			if (!isOkay) {
+				obj.selectPlayerType();
+			}
+			
+			while (gameState){
+				
+				// accept input from player
+				obj.playGame(gameRound);
+				
+				gameState = obj.checkGameState();
+				++gameRound;
+			}
+			
+			// display winner
+			obj.determineWinner(gameRound);
+			
+			// print finger history
+			obj.displayFingerHistory();
+			
+			
+			System.out.println("\n Do you want to play another game (Y,n)");
+			char playAgain = input.next().charAt(0);
+			
+			if (playAgain == 'n') {
+				//System.out.println("Thanks for playing Mora game.");
+				
+				// display game history
+				obj.displayGameHistory();
+				// display even/odd number count for each player
+				return;
+			}
+			
+		}
+
+	}
+	
+	
+
+}
